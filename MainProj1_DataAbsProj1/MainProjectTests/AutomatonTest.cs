b2cd7dc6c76ace1938bd72ae6bf219999d0adaca
mtoolee <mtoolee@hotmail.com>
2022-02-12 18:25:02 -0600

    write-test-branch initial commit

--- MainProj1_DataAbsProj1/MainProjectTests/AutomatonTest.cs
+++ MainProj1_DataAbsProj1/MainProjectTests/AutomatonTest.cs
@@ -1,13 +1,21 @@
+using MainProject;
 using Xunit;
 
 namespace MainProjectTests
 {
     public class AutomatonTest
     {
+        /*
         [Fact]
-        public void Test1()
+        public void ShouldConstruct()
         {
-
+            Automaton eca = new Automaton(22, new Generation(false, true, false));
+            Assert.Equal(22, eca.getRuleNum());
+            eca.getGeneration(0).getStates().Should().BeEquivalentTo(new bool[] { false, true, false });
+            Assert.Equal(0, eca.getTotalSteps());
+            Assert.Equal("010", eca.toString());
         }
+        */
+
     }
 }
